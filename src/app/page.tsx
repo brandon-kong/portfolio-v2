a4ce1f09af6d0b39e2549cc78ec0320376557fe2
brandon-kong <kongbrandon0@gmail.com>
2024-02-22 22:50:06 -0600

    added blog url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Button, BlogCard, H1, H2, P, Tag, BlogCardProps, SlideIn, ProjectSection } from "@/components";
 import { Briefcase, MapPin } from "react-feather";
 
-import { SITE_URL } from "@/util/variables";
+import { SITE_URL, BLOG_URL } from "@/util/variables";
 
 const blogs: BlogCardProps[] = [
     {
@@ -9,7 +9,7 @@ const blogs: BlogCardProps[] = [
         description: 'In the ever-evolving landscape of technology, AI and machine learning have emerged as pivotal elements in shaping the future of UI/UX design.',
         tags: ['Research', 'Bioinformatics'],
         image: '/blog/blog1.jpg',
-        link: `blog.${SITE_URL}/testing`,
+        link: `${BLOG_URL}/testing`,
         date: 'Aug 2023'
     },
     {
@@ -17,7 +17,7 @@ const blogs: BlogCardProps[] = [
         description: 'In the ever-evolving landscape of technology, AI and machine learning have emerged as pivotal elements in shaping the future of UI/UX design.',
         tags: ['Research', 'Bioinformatics'],
         image: '/blog/blog2.jpg',
-        link: `blog.${SITE_URL}/testing`,
+        link: `blog.${BLOG_URL}/testing`,
         date: 'Aug 2023'
     },
     {
@@ -25,7 +25,7 @@ const blogs: BlogCardProps[] = [
         description: 'In the ever-evolving landscape of technology, AI and machine learning have emerged as pivotal elements in shaping the future of UI/UX design.',
         tags: ['Research', 'Bioinformatics'],
         image: '/blog/blog3.jpg',
-        link: `blog.${SITE_URL}/testing`,
+        link: `blog.${BLOG_URL}/testing`,
         date: 'Aug 2023'
     },
 ]
--- src/util/variables.ts
+++ src/util/variables.ts
@@ -1,3 +1,4 @@
 const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
+const BLOG_URL = process.env.NEXT_PUBLIC_BLOG_URL || 'http://localhost:3000';
 
-export { SITE_URL };
+export { SITE_URL, BLOG_URL };
